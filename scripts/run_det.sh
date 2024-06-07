33b579dd3b75b1052effb31162862fab809d5dcb
crazyboy9103 <gil9103@gmail.com>
2024-06-07 04:46:57 +0000

    working on det

--- scripts/run_det.sh
+++ scripts/run_det.sh
@@ -5,23 +5,27 @@ cd /workspace/src
 lr_values=(0.0001)
 epoch_values=(12)
 noise_types=("overlook" "badloc" "swap")
-task_names=("chocoball" "bone" "cosmetic")
-batch_sizes=(16 8 64)
-min_sizes=(800 600 500)
-max_sizes=(1000 1000 500)
+# task_names=("chocoball" "cosmetic" "bone")
+# batch_sizes=(16 64 8)
+# min_sizes=(400 500 600)
+# max_sizes=(500 500 1000)
+task_names=("cosmetic" "bone")
+batch_sizes=(64 8)
+min_sizes=(500 600)
+max_sizes=(500 1000)
 task_count=${#task_names[@]}
 
-for lr in "${lr_values[@]}"; do
-    for epoch in "${epoch_values[@]}"; do
-        for noise in "${noise_types[@]}"; do
-            for i in $(seq 0 $((task_count - 1))); do
+for i in $(seq 0 $((task_count - 1))); do
+    for lr in "${lr_values[@]}"; do
+        for epoch in "${epoch_values[@]}"; do
+            for noise in "${noise_types[@]}"; do
                 task=${task_names[$i]}
                 batch_size=${batch_sizes[$i]}
                 min_size=${min_sizes[$i]}
                 max_size=${max_sizes[$i]}
                 echo "Running: LR=$lr, Epochs=$epoch, Task=$task, Noise=$noise"
-                python main_det.py model.optimizer.lr=$lr num_epochs=$epoch task_name=$task paths.data_root_dir="/datasets/conflearn/det/$task" conflearn=$noise data.batch_size=$batch_size
-                # python main_det.py model.optimizer.lr=$lr model.min_size=$min_size model.max_size=$max_size num_epochs=$epoch task_name=$task paths.data_root_dir="/datasets/conflearn/det/$task" conflearn=$noise data.batch_size=$batch_size
+                # python main_det.py model.optimizer.lr=$lr num_epochs=$epoch task_name=$task paths.data_root_dir="/datasets/conflearn/det/$task" conflearn=$noise data.batch_size=$batch_size
+                python main_det.py model.optimizer.lr=$lr model.min_size=$min_size model.max_size=$max_size num_epochs=$epoch task_name=$task paths.data_root_dir="/datasets/conflearn/det/$task" conflearn=$noise data.batch_size=$batch_size
             done
         done
     done
